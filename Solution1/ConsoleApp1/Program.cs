af0194ed7d475dc95fe940e7598b7e5e4f757a80
cankutay ucar <cankutay.ucar@commercelab.com.tr>
2023-03-27 15:08:03 +0300

    initial

---
+++ Solution1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("cankutay uçar");
+                Thread.Sleep(1000);
+            }
+
+        }
+    }
+}
